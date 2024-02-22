4c46750e77f5e97bc562a800fcb69b80dfdd4970
Demmythetechie <demmythetechie@gmail.com>
2024-02-22 15:52:51 +0100

    Updated

--- src/carts/cart.js
+++ src/carts/cart.js
@@ -10,39 +10,35 @@ function Cart() {
         </div>
         <div className="max-[885px]:w-[2.5%] w-[2.3%] aspect-[1/1] bg-[url('/public/cart.png')] bg-contain bg-no-repeat mt-[5%]"></div>
       </div>
-      <div className="grid grid-cols-[6%_18.3%_7%_18.3%_6%_18.3%_6%_18.3%_6%] items-center w-[100%] aspect-[1/0.04] pl-[3%] pr-[8%] border-b border-black">
-        <div className="flex justify-center col-[1_/_span_1] border border-black">
-          <p className="max-[885px]:text-[1.5vw] text-[1.2vw] font-medium">Item</p>
+      <div className="max-[530px]:hidden brk grid grid-cols-[6%_18.3%_7%_18.3%_6%_18.3%_6%_18.3%_6%] items-center w-[100%] aspect-[1/0.04] pl-[2.5%] pr-[9.5%] border-b border-black">
+        <div className="flex justify-center col-[1_/_span_1]">
+          <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] text-[1.2vw] font-medium">Item</p>
         </div>
-        <div className="col-[2_/_span_1] border border-black"></div>
-        <div className="flex justify-center col-[3_/_span_1] border border-black">
-          <p className="max-[885px]:text-[1.5vw] text-[1.2vw] font-medium">Quantity</p>
+        <div className="flex justify-center col-[3_/_span_1]">
+          <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] text-[1.2vw] font-medium">Quantity</p>
         </div>
-        <div className="col-[4_/_span_1] border border-black"></div>
-        <div className="flex justify-center col-[5_/_span_1] border border-black">
-          <p className="max-[885px]:text-[1.5vw] text-[1.2vw] font-medium">Color</p>
+        <div className="flex justify-center col-[5_/_span_1]">
+          <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] text-[1.2vw] font-medium">Color</p>
         </div>
-        <div className="col-[6_/_span_1] border border-black"></div>
-        <div className="flex justify-center col-[7_/_span_1] border border-black">
-          <p className="max-[885px]:text-[1.5vw] text-[1.2vw] font-medium">Size</p>
+        <div className="flex justify-center col-[7_/_span_1]">
+          <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] text-[1.2vw] font-medium">Size</p>
         </div>
-        <div className="col-[8_/_span_1] border border-black"></div>
-        <div className="flex justify-start col-[9_/_span_1] border border-black">
-          <p className="max-[885px]:text-[1.5vw] text-[1.2vw] font-medium">Price</p>
+        <div className="flex justify-center col-[9_/_span_1]">
+          <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] text-[1.2vw] font-medium">Price</p>
         </div>
       </div>
       <Items />
       <div className="flex flex-col gap-y-[20px] w-[100%] aspect-[1/0.1] border-b border-black items-end justify-center">
         <div className="w-[32%] justify-between items-center flex flex-row">
-            <p className="font-semibold text-[1.3vw]">Subtotal:</p>
+            <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] font-semibold text-[1.3vw]">Subtotal:</p>
             <div className="w-[34%]">
-                <p className="text-[1.1vw] font-semibold">$<span>2323</span></p>
+                <p className="max-[885px]:text-[1.3vw] max-[730px]:text-[1.5vw] max-[630px]:text-[1.7vw] text-[1.1vw] font-semibold">$<span>2323</span></p>
             </div>
         </div>
         <div className="w-[32%] justify-between items-center flex gap-x-[7%] flex-row">
-            <p className="font-semibold text-[1.3vw]">Delivery:</p>
+            <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] font-semibold text-[1.3vw]">Delivery:</p>
             <div className="w-[34%]">
-                <p className="text-[1.1vw] font-semibold">$<span>23</span></p>
+                <p className="max-[885px]:text-[1.3vw] max-[730px]:text-[1.5vw] max-[630px]:text-[1.7vw] text-[1.1vw] font-semibold">$<span>23</span></p>
             </div>
         </div>
       </div>
@@ -58,16 +54,49 @@ function Items() {
   return (
     <>
         {items.map((item)=>(
-            <div className="flex flex-row justify-between items-center w-[100%] aspect-[1/0.08] max-[885px]:aspect-[1/0.1] pl-[2.5%] pr-[8.1%] border-b border-black">
-                <div className={`${item.img} max-[885px]:w-[8%] w-[6.5%] aspect-[1/1] bg-contain bg-no-repeat border border-black mr-[-2.8%]`}></div>
-                <div className="max-[885px]:w-[8%] flex flex-row w-[6%] justify-between items-center pl-[7px] pr-[7px] border border-black">
-                    <button className="max-[885px]:text-[1.7vw] text-[1.3vw] font-semibold pb-[3px]">-</button>
-                    <p className="max-[885px]:text-[1.4vw] text-[1.1vw] font-regular">{item.quantity}</p>
-                    <button className="max-[885px]:text-[1.7vw] text-[1.3vw] font-semibold pb-[3px]">+</button>
+            <div className="brk max-[530px]:flex max-[530px]:flex max-[530px]:flex-row max-[530px]:justify-between max-[530px]:aspect-[1/0.23] grid grid-cols-[6%_18.3%_7%_18.3%_6%_18.3%_6%_18.3%_6%] items-center w-[100%] aspect-[1/0.08] max-[885px]:aspect-[1/0.1] pl-[2.5%] pr-[9.5%] border-b border-black">
+                <div className={`max-[530px]:w-[20%] col-[1_/_span_1] ${item.img} aspect-[1/1] bg-contain bg-no-repeat border border-black mr-[-2.8%]`}></div>
+                <div className="max-[530px]:w-[15%] col-[3_/_span_1] flex flex-row justify-between items-center pl-[7px] pr-[7px] border border-black">
+                    <button className="max-[885px]:text-[1.7vw] max-[730px]:text-[1.9vw] max-[630px]:text-[2.1vw] max-[530px]:text-[3vw] text-[1.3vw] font-semibold pb-[3px]">-</button>
+                    <p className="max-[885px]:text-[1.4vw] max-[730px]:text-[1.6vw] max-[630px]:text-[1.8vw] max-[530px]:text-[2.6vw] text-[1.1vw] font-regular">{item.quantity}</p>
+                    <button className="max-[885px]:text-[1.7vw] max-[730px]:text-[1.9vw] max-[630px]:text-[2.1vw] max-[530px]:text-[3vw] text-[1.3vw] font-semibold pb-[3px]">+</button>
                 </div>
-                <p className="max-[885px]:text-[1.5vw] font-regular text-[1.2vw] ml-[-0.7%]">{item.color}</p>
-                <p className="max-[885px]:text-[1.5vw] font-regular text-[1.2vw]">{item.size}</p>
-                <p className="max-[885px]:text-[1.5vw] font-regular text-[1.2vw]"><span>$</span>{item.price}</p>
+                <div className="max-[530px]:hidden flex justify-center col-[5_/_span_1]">
+                    <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] font-regular text-[1.2vw] ml-[-0.7%]">{item.color}</p>
+                </div>
+                <div className="max-[530px]:hidden flex justify-center col-[7_/_span_1]">
+                    <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] font-regular text-[1.2vw]">{item.size}</p>
+                </div>
+                <div className="max-[530px]:hidden flex justify-center col-[9_/_span_1]">
+                    <p className="max-[885px]:text-[1.5vw] max-[730px]:text-[1.7vw] max-[630px]:text-[1.9vw] font-regular text-[1.2vw]"><span>$</span>{item.price}</p>
+                </div>
+
+                {/*for 530px */}
+
+                <div className="max-[530px]:flex hidden w-[40%] max-[530px]:flex-col border border-black">
+                    <div className="w-[100%] justify-between items-center flex flex-row">
+                        <p className="font-regular text-[2.2vw]">Color:</p>
+                        <div className="w-[34%]">
+                            <p className="font-regular text-[2.2vw]"><span></span>{item.color}</p>
+                        </div>
+                    </div>
+                    <div className="w-[100%] justify-between items-center flex flex-row">
+                        <p className="font-regular text-[2.2vw]">Color:</p>
+                        <div className="w-[34%]">
+                            <p className="font-regular text-[2.2vw]"><span></span>{item.color}</p>
+                        </div>
+                    </div>
+                    <div className="w-[100%] justify-between items-center flex flex-row">
+                        <p className="font-regular text-[2.2vw]">Color:</p>
+                        <div className="w-[34%]">
+                            <p className="font-regular text-[2.2vw]"><span></span>{item.color}</p>
+                        </div>
+                    </div>
+                    
+                    
+                    <p className="font-regular text-[2.2vw]"><span>Size:</span>{item.size}</p>
+                    <p className="font-regular text-[2.2vw]"><span>Price:</span>{item.price}</p>
+                </div>
           </div>
         ))}
     </>

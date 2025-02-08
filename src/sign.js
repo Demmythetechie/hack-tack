9dc7a89f10ee9c8d16d85572624d314a2f6c72e1
Demmythetechie <demmythetechie@gmail.com>
2025-02-08 15:10:59 +0100

    updated signin page

--- src/sign.js
+++ src/sign.js
@@ -5,6 +5,9 @@ import axios from "axios";
 function SignUp() {
 
     const [signin, updateIn] = useState(false);
+    const [exist, setExist] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [notExist, setNotExist] = useState(false);
     const [err, setErr] = useState(false);
     function login() {
         (signin === true) ? updateIn(false) : updateIn(true);
@@ -13,6 +16,8 @@ function SignUp() {
 
     async function signup(e) {
         e.preventDefault(); // Prevent the default form submission behavior
+        setLoading(true);
+
 
         const formData = new FormData(e.target); // Collect form data
         const formValues = Object.fromEntries(formData.entries()); // Convert to a plain object
@@ -21,22 +26,22 @@ function SignUp() {
         console.log('Form Submitted:', formValues);
 
         if (formValues.pswd === formValues.cpswd) {
-
-            try {
-                const response = await axios.post('https://example.com/api/signup', formValues, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-
-                // Handle successful response
-                console.log('Response:', response.data);
-                alert('Form submitted successfully!');
-            } catch (error) {
-                // Handle error
-                console.error('Error submitting form:', error);
+            await axios.post('https://hack-tack-api.onrender.com/signup', formValues, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response)=>{
+                console.log(response.data);
+                setLoading(false);
+                (response.data === 'An account has been created with this email') ? setExist(true) : setExist(false);
+                (response.data === 'Saved Succefully') ? setNotExist(true) : setNotExist(false);
+            })
+            .catch((error)=>{
+                console.log(error);
+                setLoading(false);
                 alert('An error occurred while submitting the form.');
-            }
+            });
         } else {
             setErr(true);
         }
@@ -44,12 +49,12 @@ function SignUp() {
 
         return (
             <section className="flex flex-row w-[100%] h-[90vh] min-[530px]:h-[100vh]">
-                <div className="w-[65%] min-[530px]:w-[40%] sm:w-[100%] md:w-[100%] lg:w-[65%] xl:w-[65%] h-[100%] hidden min-[530px]:block">
+                <div className={`${(notExist) ? 'hidden' : 'min-[530px]:block'} w-[65%] min-[530px]:w-[40%] sm:w-[100%] md:w-[100%] lg:w-[65%] xl:w-[65%] h-[100%] hidden`}>
                     <video className="w-[100%] h-[100%] object-cover object-[50%_35%]" muted autoPlay loop>
                         <source src="pants.mp4" type="video/mp4" />
                     </video>
                 </div>
-                <form onSubmit={signup} className="w-[100%] min-[530px]:w-[60%] sm:w-[55%] md:w-[45%] lg:w-[35%] xl:w-[35%] h-[100%] sm:h-[95%] md:h-[95%] lg:h-[100%] xl:h-[100%] flex flex-col items-center justify-center gap-y-[4%] relative min-[530px]:relative sm:absolute md:absolute lg:relative xl:relative bg-white/50  sm:top-[2.5%] sm:left-[20%] md:top-[2.5%] md:left-[27.5%] lg:top-[0%] lg:left-[0%] xl:top-[0%] xl:left-[0%]">
+                <form onSubmit={signup} className={`${(notExist) ? 'hidden' : 'flex'} w-[100%] min-[530px]:w-[60%] sm:w-[55%] md:w-[45%] lg:w-[35%] xl:w-[35%] h-[100%] sm:h-[95%] md:h-[95%] lg:h-[100%] xl:h-[100%] flex-col items-center justify-center gap-y-[4%] relative min-[530px]:relative sm:absolute md:absolute lg:relative xl:relative bg-white/50  sm:top-[2.5%] sm:left-[20%] md:top-[2.5%] md:left-[27.5%] lg:top-[0%] lg:left-[0%] xl:top-[0%] xl:left-[0%]`}>
                     <div className="w-[10%] aspect-square">
                         <svg viewBox="0 0 150 150">
                             <path d="M0 1 L8 1 M8 1 L8 100 M8 100 L98 100 M98 100 L98 149 M8 120 L8 149 M0 149 L60 149 M52 149 L52 50 M52 50 L100 50 M100 50 L100 1 M100 1 L92 1 M149 149 L90 149 M142 149 L142 1 M149 1 L120 1 M120 1 L120 70 M120 70 L30 70 M30 70 L30 1 M30 1 L60 1 M52 1 L52 20" stroke="#000" strokeWidth="2px" />
@@ -65,18 +70,32 @@ function SignUp() {
                         <input className="border-b border-black w-[100%] aspect-[1/0.1] max-[1280px]:h-[70%] placeholder:font-[Montserrat] placeholder:text-sm sm:placeholder:text-sm md:placeholder:sm lg:placeholder:text-sm xl:placeholder:text-sm font-[Montserrat] text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm placeholder:font-normal focus:outline-none focus:border-b focus:border-black placeholder:text-black bg-transparent" type="password" id="pswd" name="pswd" placeholder="Enter Password" pattern="^(?=(.*[a-z]))(?=(.*[A-Z]))(?=(.*\d))(?=(.*[\W_])).{8,}$" title="Must contain atleast 1 uppercase, 1 lowercase, 1 numerical character and 1 symbol. Minimum of 8 character." required />
                         <input className={`${signin === true ? 'hidden' : 'flex'} border-b border-black w-[100%] aspect-[1/0.1] max-[1280px]:h-[70%] placeholder:font-[Montserrat] placeholder:text-sm sm:placeholder:text-sm md:placeholder:sm lg:placeholder:text-sm xl:placeholder:text-sm font-[Montserrat] text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm placeholder:font-normal focus:outline-none focus:border-b focus:border-black placeholder:text-black bg-transparent`} type="password" id="cpswd" name="cpswd" placeholder="Confirm Password" pattern="^(?=(.*[a-z]))(?=(.*[A-Z]))(?=(.*\d))(?=(.*[\W_])).{8,}$" title="Must contain atleast 1 uppercase, 1 lowercase, 1 numerical character and 1 symbol. Minimum of 8 character." required />
                         {err !== null ?
-                            <p className={`${(err === true ? 'inline-block' : 'hidden')} font-[ubuntu] font-extralight sm:text-xl md:text-xl lg:text-sm xl:text-sm`}>The password confirmation does not match</p>
+                            <p className={`${(err === true ? 'inline-block' : 'hidden')} font-[ubuntu] font-semibold sm:text-xl md:text-xl lg:text-sm xl:text-sm text-red-500`}>The password confirmation does not match</p>
                         :
                             <></>
                         }
+                        <p className={`${(exist === true ? 'inline-block' : 'hidden')} font-[ubuntu] font-semibold sm:text-xl md:text-xl lg:text-sm xl:text-sm text-red-500`}>An account has been created with this email</p>
                         <div className={`${signin === true ? 'hidden' : 'flex'} w-[100%] gap-x-2`}>
                             <input className="font-[ubuntu] text-black" type="checkbox" id="terms" name="terms" required></input>
                             <p className="font-[ubuntu] font-extralight sm:text-xl md:text-xl lg:text-sm xl:text-sm">I agree to the <span className="text-blue-800 underline">terms & conditions</span></p>
                         </div>
-                        <input className="w-[100%] aspect-[1/0.12] max-[1280px]:h-[70%] pl-[3%] font-[ubuntu] text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm font-semibold focus:outline-none focus:border focus:border-black bg-black text-white" type="submit" id="done" name="done" value={`${signin === true ? 'Sign In' : 'Create account'}`} />
+                        <input className={`${(loading) ? 'hidden' : 'block'} w-[100%] aspect-[1/0.12] max-[1280px]:h-[70%] pl-[3%] font-[ubuntu] text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm font-semibold focus:outline-none focus:border focus:border-black bg-black text-white`} type="submit" id="done" name="done" value={`${signin === true ? 'Sign In' : 'Create account'}`} />
+                        <div className={`${(loading) ? 'flex' : 'hidden'} w-[100%] aspect-[1/0.12] max-[1280px]:h-[70%] pl-[3%] bg-black items-center justify-center`}>
+                            <div className="animate-spin rounded-full w-[5%] aspect-square border-t-2 border-r-2 border-white border-solid"></div>
+                        </div>
                         <p className="font-[ubuntu] font-extralight sm:text-xl md:text-xl lg:text-sm xl:text-sm">{signin === true ? 'Don\'t  have an account?' : 'Already have an account?'}<span onClick={login} className="text-blue-800 underline cursor-pointer">{signin === true ? 'Create an account' : 'Log in'}</span></p>
                     </div>
                 </form>
+                <div className={`${(notExist) ? 'flex' : 'hidden'} w-[100%] h-[100%] flex-col items-center justify-center`}>
+                    <div className="w-[15%] sm:w-[10%] md:w-[7%] lg:w-[5%] xl:w-[4%] aspect-square">
+                        <svg viewBox="0 0 150 150">
+                            <path d="M0 1 L8 1 M8 1 L8 100 M8 100 L98 100 M98 100 L98 149 M8 120 L8 149 M0 149 L60 149 M52 149 L52 50 M52 50 L100 50 M100 50 L100 1 M100 1 L92 1 M149 149 L90 149 M142 149 L142 1 M149 1 L120 1 M120 1 L120 70 M120 70 L30 70 M30 70 L30 1 M30 1 L60 1 M52 1 L52 20" stroke="#000" strokeWidth="2px" />
+                        </svg>
+                    </div>
+                    <p className="font-[Montserrat] font-normal sm:text-xl md:text-xl lg:text-2xl xl:text-3xl text-green-800">Account has been created succesfully</p>
+                    <p className="font-[Montserrat] font-light sm:text-xl md:text-xl lg:text-lg xl:text-lg text-center">Kindly check your email to verify your account</p>
+                    <p className="font-[Montserrat] font-light sm:text-xl md:text-xl lg:text-lg xl:text-lg text-center">The email is valid for 3 days</p>
+                </div>
             </section>
         );
     }
